a90b0e14ec94c941e43fd37b8f52751dc04cf03b
hemraj97 <mahadeshwar.h@northeastern.edu>
2024-04-15 13:34:08 -0400

    Changes for frontend header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,14 +23,15 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-red-500 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
-        </Link>
+      <Link to='/' className="flex items-center"> {/* Ensure flex container */}
+      <img src="/images/nu_logo.png" alt="NU Logo" className="h-8 sm:h-10 mr-2" /> {/* Add margin to the right */}
+      <h1 className='font-bold text-black sm:text-xl flex flex-wrap'>
+        <span >NEU</span>
+        <span >Apartments</span>
+      </h1>
+    </Link>
         <form
           onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'
@@ -48,12 +49,12 @@ export default function Header() {
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline font-bold text-black hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline font-bold text-black hover:underline'>
               About
             </li>
           </Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,14 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find the coolest student spots to <span className='text-slate-500'>live</span>
           <br />
-          place with ease
+         with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
+          NEU Apartments is a apartments listing page for the students, by the students and from the students of Northeastern University.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of properties for you to choose from around the Boston Campus.
         </div>
         <Link
           to={'/search'}
